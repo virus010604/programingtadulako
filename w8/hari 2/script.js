131dbd25ff51f33116d02baa7568fa8be22c0a00
virus01064 <jp1jn04@gmail.com>
2024-03-16 21:19:07 +0800

    ubah sementara

---
+++ w8/hari 2/script.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+
+let a = 3
+let b = 3
+let c = true
+a = false
+
+const nama = a + b + c;
+
+console.log(typeof a);
+
+const isRaining = false;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (isRaining) {
+    console.log("Hari ini hujan. Bawa payung.");
+}
+    console.log("Berangkat kegiatan.");
+
+let language = "English";
+let greeting = "Selamat Pagi"
+
+if(language === "English") {
+    greeting = "Good Morning!";
+} else if(language === "French") {
+    greeting = "Bonjour!"
+} else if(language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+}
+
+console.log(greeting);
+
+let v = prompt("masukan nama anda: ");
+console.log(v);
+
+
+
